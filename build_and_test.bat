52a92726bdac687fc4d0b08e66e3e42f01c7fd51
Conrad Korzon <conrad.korzon@gmail.com>
2026-03-22 15:22:30 -0400

    fix: Implement basic ColorSchemes

--- build_and_test.bat
+++ build_and_test.bat
@@ -1,5 +1,6 @@
 cmake -B build -S . --preset=msvc-release
 cmake --build build --config Release
-.\build\tests\Release\test_canvas.exe
-.\build\tests\Release\test_geometry.exe
+.\build\tests\Release\test_colorschemes.exe
 .\build\tests\Release\test_shapes.exe
+.\build\tests\Release\test_geometry.exe
+.\build\tests\Release\test_canvas.exe
